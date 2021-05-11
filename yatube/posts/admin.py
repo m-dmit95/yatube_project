7793c534698f486eecd83da7da7b11b58262a616
m-dmit95 <m.dmit95@gmail.com>
2021-05-11 20:43:58 +0700

    Прошел теоретическую часть спринта. Коммит перед выполнением проектного задания.

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("pk", "text", "pub_date", "author")
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",)
+    # добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",)
+    # добавляем заглушку для записей без текста
+    empty_value_display = "-пусто-"
+
+# при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
+admin.site.register(Post, PostAdmin)
